c5105533822803752372eccf33a40c8f5fb281da
Cesar Richard <cesar.richard@weezevent.com>
2020-10-17 18:52:53 +0200

    changed port

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("combined"));
 app.use(myRouter);
-app.listen(80, "0.0.0.0", function() {
+app.listen(443, "0.0.0.0", function() {
   console.log("Listening");
 });
 const pipeline = [
